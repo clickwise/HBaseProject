6361cbf9f001249026a463a1dbd3e0e249e377a5
clickwise <strongmind666@126.com>
2015-04-11 17:50:00 +0800

    run test

---
+++ src/cn/clickwise/clickad/hbase/ITServer.java
@@ -0,0 +1,165 @@
+package cn.clickwise.clickad.hbase;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.text.MessageFormat.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import cn.clickwise.lib.string.SSO;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+
+/**
+ * 根据ip和时间查询radiusId
+ * @author zkyz
+ */
+public class ITServer implements Runnable{
+	
+	private Properties properties = new Properties();
+	static Logger logger = LoggerFactory.getLogger(ITServer.class);
+	
+	ELITSRadiusStore elits=new ELITSRadiusStore();
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		// 配置成根据传入请求的前缀不同调用不同的处理程序
+		// 每种请求对应一个handler
+		
+		try {
+			HttpServer hs = HttpServer.create(new InetSocketAddress(Integer.parseInt(properties.getProperty("port"))), 0);
+
+
+			ITHandler test_handler = new ITHandler();
+			hs.createContext("/ipq", test_handler);
+
+			hs.setExecutor(null);
+			hs.start();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	private class ITHandler implements HttpHandler{
+		
+		@Override
+		public void handle(HttpExchange exchange) throws IOException {
+			
+			String uri = exchange.getRequestURI().toString();
+			System.out.println("uri:"+uri);
+			uri = uri.replaceFirst("\\/ipq\\?", "");
+			HashMap<String,String> phash=convertParams(uri);
+			String ip="";
+			String time="";
+			ip=phash.get("ip");
+			time=phash.get("time");
+			
+			List<String> radiusIds=elits.get(ip, time);
+			
+			String restr="";
+			if(radiusIds==null)
+			{
+				restr="nothing";
+			}
+			else
+			{
+			  for(int i=0;i<radiusIds.size()-1;i++)
+			  {
+				 restr=restr+radiusIds.get(i)+",";
+			  }
+			  restr=restr+radiusIds.get(radiusIds.size()-1);
+			}
+			//encode_res = encode_res.replaceAll("\\s+", "");	
+			//exchange.sendResponseHeaders(200, encode_res.length());
+			exchange.sendResponseHeaders(200,0);
+			OutputStream os = exchange.getResponseBody();
+			OutputStreamWriter osw=new OutputStreamWriter(os,"gbk");
+			PrintWriter pw=new PrintWriter(osw);
+			//os.write(encode_res.getBytes());
+			pw.println(restr);
+			pw.flush();
+			pw.close();
+			os.close();
+			
+		}	
+	}
+	
+	public HashMap<String,String> convertParams(String param_str)
+	{
+		String[] fields=param_str.split("&");
+		if(fields==null||fields.length<1)
+		{
+			return null;
+		}
+		
+		HashMap<String,String> phash=new HashMap<String,String>();
+		String key="";
+		String value="";
+		
+		for(int i=0;i<fields.length;i++)
+		{
+		  key=SSO.beforeStr(fields[i], "=");
+		  value=SSO.afterStr(fields[i], "=");
+		  if(SSO.tioe(key)||SSO.tioe(value))
+		  {
+			  continue;
+		  }
+		  
+		  phash.put(key, value);
+		}
+		
+		return phash;
+	}
+	
+	
+	public void read_input_parameters(String[] args) {
+		int i;
+		for (i = 0; (i < args.length) && ((args[i].charAt(0)) == '-'); i++) {
+			switch ((args[i].charAt(1))) {
+			case 'h':
+				print_help();
+				System.exit(0);
+			case 'p':
+				i++;
+				properties.setProperty("port", args[i]);
+				break;			
+			default:
+				System.out.println("Unrecognized option " + args[i] + "!");
+				print_help();
+				System.exit(0);
+			}
+		}
+
+		System.out.println(properties.toString());
+	}
+	
+	public static void print_help() {
+		System.out.println("usage: ITServer [options]");
+		System.out.println("options: -h  -> this help");
+		System.out.println("         -p  server port");
+	}
+	
+	public static void main(String[] args)
+	{
+		ITServer cps=new ITServer();
+		cps.read_input_parameters(args);
+		Thread serverThread = new Thread(cps);
+		serverThread.start();	
+	}
+	
+}
