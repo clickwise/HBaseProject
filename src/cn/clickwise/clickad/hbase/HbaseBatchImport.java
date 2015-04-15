87d519c7fcc61ed3905a72dc7c6fa6c3013096f1
clickwise <strongmind666@126.com>
2015-04-15 16:50:04 +0800

    run test

--- src/cn/clickwise/clickad/hbase/HbaseBatchImport.java
+++ src/cn/clickwise/clickad/hbase/HbaseBatchImport.java
@@ -1,7 +1,9 @@
 package cn.clickwise.clickad.hbase;
 
 import java.text.SimpleDateFormat;  
+import java.util.ArrayList;
 import java.util.Date;  
+import java.util.List;
   
 import org.apache.hadoop.conf.Configuration;  
 import org.apache.hadoop.hbase.client.Put;  
@@ -16,6 +18,9 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;  
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;  
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;  
+
+import cn.clickwise.lib.code.MD5Code;
+import cn.clickwise.lib.time.TimeOpera;
 
 public class HbaseBatchImport {
 
@@ -44,6 +49,11 @@ public class HbaseBatchImport {
 
 	static class BatchImportReducer extends
 			TableReducer<LongWritable, Text, NullWritable> {
+		
+		public static String RID = "rid";
+		public static String OIP = "oip";
+		public static String TNAME = "hradius";
+		
 		protected void reduce(LongWritable key,
 				java.lang.Iterable<Text> values, Context context)
 				throws java.io.IOException, InterruptedException {
@@ -57,8 +67,49 @@ public class HbaseBatchImport {
 				context.write(NullWritable.get(), put);
 			}
 		};
-	}
+		
+		public List<Put> getPut(String record)
+		{
+			String ip = "";
+			String status = "";
+			String radiusid = "";
+			long time = 0;
+			String time_str="";
+			
+			String[] fields = record.split("\\s+");
+			if (fields.length < 1) {
+				return null;
+			}
 
+			ip = fields[0];
+
+			String md5ip = MD5Code.makeMD5(ip);
+
+			List<Put> list=new ArrayList<Put>();
+			for (int j = 1; j < fields.length; j++) {
+				
+			    String[] tokens=null;
+			    tokens=fields[j].split(":");
+			    if(tokens.length!=3)
+			    {
+			    	continue;
+			    }
+			    
+			    time=Long.parseLong(tokens[0]);
+			    radiusid=tokens[1];
+			    status=tokens[2];	    
+			    time_str=TimeOpera.long2strm(time);
+			   		    		 		
+				String rowkey = md5ip + time_str.replaceAll("\\s+", "") + status;
+				Put put = new Put(rowkey.getBytes());
+				put.add(RID.getBytes(), "c".getBytes(), radiusid.getBytes());
+				put.add(OIP.getBytes(), "c".getBytes(), ip.getBytes());
+				list.add(put);
+		    }
+			
+			return list;
+	   }
+	}
 	public static void main(String[] args) throws Exception {
 		final Configuration configuration = new Configuration();
 		// 设置zookeeper
@@ -82,7 +133,7 @@ public class HbaseBatchImport {
 		// 不再设置输出路径，而是设置输出格式类型
 		job.setOutputFormatClass(TableOutputFormat.class);
 
-		FileInputFormat.setInputPaths(job, "hdfs://192.168.10.107:8020/input");
+		FileInputFormat.setInputPaths(job, "hdfs://192.168.10.107:8020/user/clickwise/hradiusInput");
 
 		job.waitForCompletion(true);
 	}
