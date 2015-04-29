eb46c24fdd3ce5bf87a6aa4d8612d6c445f5d70e
GitLab <gitlab@localhost>
2015-04-29 09:46:26 +0800

    run test

--- run_cmd.sh
+++ run_cmd.sh
@@ -7,7 +7,7 @@ INPUT=input_temp
 for LINE in `ls $INPUT/$DAY`  
 do  
         ./radius_hbase $INPUT/$DAY/$LINE > radius_parse/$DAY/$LINE   
-        cat radius_parse/$DAY/$LINE | java -Xmx3000m -cp out/myhbase.jar cn.clickwise.clickad.hbase.ELITSRadiusStore add $DAY         
+        cat radius_parse/$DAY/$LINE | java -Xmx3000m -cp out/myhbase.jar cn.clickwise.clickad.hbase.OELITSRadiusStore add $DAY         
         echo $LINE 
 done
 #cat t.txt | java -Xmx3000m -XX:MaxPermSize=300m -cp out/myhbase.jar cn.clickwise.clickad.hbase.ELITSRadiusStore add 2015-04-05 > logaddd.txt 2>&1 &
