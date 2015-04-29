af8b1fb18de1715dbab6024c6119ca53043d4947
clickwise <strongmind666@126.com>
2015-04-29 09:24:29 +0800

    run test

--- src/cn/clickwise/clickad/hbase/OELITSRadiusStore.java
+++ src/cn/clickwise/clickad/hbase/OELITSRadiusStore.java
@@ -56,17 +56,17 @@ public class OELITSRadiusStore extends RadiusStore {
 		 * configuration.set("hbase.master", "192.168.10.103:60000");
 		 ********************************/
 
-		/************local*******************/
+		/************local*******************
 		 configuration.set("hbase.zookeeper.property.clientPort", "2181");
 		 configuration.set("hbase.zookeeper.quorum", "192.168.110.80");
 		 configuration.set("hbase.master", "192.168.110.80:60000");
-		/************************************/
+		************************************/
 
-		/************ zj *****************
+		/************ zj *****************/
 		configuration.set("hbase.zookeeper.property.clientPort", "2181");
 		configuration.set("hbase.zookeeper.quorum", "192.168.10.130");
 		configuration.set("hbase.master", "192.168.10.128:60010");
-		********************************/
+		/********************************/
 
 		pool = new HTablePool(configuration, 100);
 		String[] cfs = { RID, OIP };
@@ -215,7 +215,6 @@ public class OELITSRadiusStore extends RadiusStore {
 
 			pool.getTable(TNAME).setAutoFlush(autoFlush);
 			if (writeBuffer != 0) {
-
 				pool.getTable(TNAME).setWriteBufferSize(writeBuffer);
 			}
 
